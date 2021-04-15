bb88be559473bd21ee67e0334be8a3bf870f6694
bibin22 <bibinjoy82@gmail.com>
2021-04-15 20:48:49 +0530

    class based view

--- app_modelserializer/urls.py
+++ app_modelserializer/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import article_list, article_detail
 from .apiview import api_article_list, api_article_details
+from .class_views import ArticleAPIView, ArticlesDetails
 
 urlpatterns = [
     path('', article_list),
     path('detail/<int:id>', article_detail),
     path('api_list', api_article_list),
-    path('api_details/<int:id>', api_article_details)
+    path('api_details/<int:id>', api_article_details),
+    path('api_class_list', ArticleAPIView.as_view()),
+    path('api_class_details/<int:id>', ArticlesDetails.as_view()),
 
 ]
