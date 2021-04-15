a92e7aab2cb2e0daccd00621c56b678dbb317311
bibin22 <bibinjoy82@gmail.com>
2021-04-15 19:29:36 +0530

    api view decorator

--- app_modelserializer/serializers.py
+++ app_modelserializer/serializers.py
@@ -3,4 +3,5 @@ from app_RestBasics.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author']
+        #fields = ['id', 'title', 'author']
+        fields = "__all__"
--- app_modelserializer/urls.py
+++ app_modelserializer/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import article_list, article_detail
+from .apiview import api_article_list, api_article_details
 
 urlpatterns = [
     path('', article_list),
     path('detail/<int:id>', article_detail),
+    path('api_list', api_article_list),
+    path('api_details/<int:id>', api_article_details)
 
 ]
