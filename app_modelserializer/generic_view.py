f8f72e02633c2156751be1ce5e66bb0c5965a9ab
bibin22 <bibinjoy82@gmail.com>
2021-04-15 21:44:42 +0530

    generic view

---
+++ app_modelserializer/generic_view.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from app_RestBasics.models import Article
+from .serializers import ArticleSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+
+class GenericAPIViews(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+class GenericDetails(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
+    def get(self, request, id = None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
--- app_modelserializer/urls.py
+++ app_modelserializer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import article_list, article_detail
 from .apiview import api_article_list, api_article_details
 from .class_views import ArticleAPIView, ArticlesDetails
-
+from .generic_view import GenericAPIViews, GenericDetails
 urlpatterns = [
     path('', article_list),
     path('detail/<int:id>', article_detail),
@@ -10,5 +10,7 @@ urlpatterns = [
     path('api_details/<int:id>', api_article_details),
     path('api_class_list', ArticleAPIView.as_view()),
     path('api_class_details/<int:id>', ArticlesDetails.as_view()),
+    path('generic_list', GenericAPIViews.as_view()),
+    path('generic_details/<int:id>', GenericDetails.as_view())
 
 ]
