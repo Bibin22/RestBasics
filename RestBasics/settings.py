c314c07e4af88d1cf6a20f9c83c56e9e83543134
bibin22 <bibinjoy82@gmail.com>
2021-04-15 15:00:54 +0530

    model serializer

--- RestBasics/settings.py
+++ RestBasics/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'app_RestBasics',
+    'app_modelserializer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
